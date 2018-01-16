fabe58f4b7e11db8fe86b0f6d62a2c21c3af9eb8
deadcheat <deadcheat@users.noreply.github.com>
2018-01-17 00:26:55 +0900

    separate interface

--- interfaces.go
+++ interfaces.go
@@ -6,11 +6,12 @@ import (
 
 // CouchBaseAdaptor CouchBase connect adaptor
 type CouchBaseAdaptor interface {
-	Open() (*gocb.Cluster, error)
-	Env() *Config
-	Cluster() *gocb.Cluster
-	AddBucket(name string) (*gocb.Bucket, error)
-	Bucket(name string) (*gocb.Bucket, error)
+	Configurable
+	Operatable
+}
+
+// Operatable abstracted for operating on couchbase
+type Operatable interface {
 	Get(key string, data interface{}) (gocb.Cas, error)
 	Insert(key string, data interface{}, expire uint32) (gocb.Cas, error)
 	Upsert(key string, data interface{}, expire uint32) (gocb.Cas, error)
@@ -22,7 +23,10 @@ type CouchBaseAdaptor interface {
 // Configurable governs for configure and open connect
 type Configurable interface {
 	Open() error
-	Env() *Config
+	Config() *Config
+	Cluster() *gocb.Cluster
+	AddBucket(name string) (*gocb.Bucket, error)
+	Bucket(name string) (*gocb.Bucket, error)
 }
 
 // Loggerable logging interface
